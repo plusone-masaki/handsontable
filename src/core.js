e6dfbd4f838b8bd9727f456de140a9da120919be
m_enomoto <m_enomoto@plus-one.tech>
2025-11-12 14:43:19 +0900

    - 既存の脆弱性を修正 - 機能追加に向けた仕様書の作成

--- src/core.js
+++ src/core.js
@@ -23,7 +23,7 @@ import { getPlugin } from './plugins';
 import { getRenderer } from './renderers';
 import { getValidator } from './validators';
 import { randomString } from './helpers/string';
-import { rangeEach, rangeEachReverse } from './helpers/number';
+import { rangeEach, rangeEachReverse, isNumericLike } from './helpers/number';
 import TableView from './tableView';
 import DataSource from './dataSource';
 import { translateRowsToColumns, cellMethodLookupFactory, spreadsheetColumnLabel } from './helpers/data';
@@ -893,8 +893,6 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
 
   function validateChanges(changes, source, callback) {
     const waitingForValidator = new ValidatorsQueue();
-    const isNumericData = value => value.length > 0 && /^\s*[+-.]?\s*(?:(?:\d+(?:(\.|,)\d+)?(?:e[+-]?\d+)?)|(?:0x[a-f\d]+))\s*$/.test(value);
-
     waitingForValidator.onQueueEmpty = resolve;
 
     for (let i = changes.length - 1; i >= 0; i--) {
@@ -905,7 +903,7 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
         const col = datamap.propToCol(prop);
         const cellProperties = instance.getCellMeta(row, col);
 
-        if (cellProperties.type === 'numeric' && typeof newValue === 'string' && isNumericData(newValue)) {
+        if (cellProperties.type === 'numeric' && typeof newValue === 'string' && isNumericLike(newValue)) {
           changes[i][3] = getParsedNumber(newValue);
         }
 
--- src/helpers/number.js
+++ src/helpers/number.js
@@ -1,18 +1,48 @@
+/* eslint-disable jsdoc/require-description-complete-sentence */
 /**
- * Checks if value of n is a numeric one
- * http://jsperf.com/isnan-vs-isnumeric/4
- * @param n
+ * Checks if the passed value is numeric one.
+ *
+ * @param {*} value The value to check.
+ * @param {string[]} additionalDelimiters An additional delimiters to be used while checking the numeric value.
  * @returns {boolean}
  */
-export function isNumeric(n) {
-  /* eslint-disable */
-  var t = typeof n;
+export function isNumeric(value, additionalDelimiters = []) {
+  const type = typeof value;
 
-  return t == 'number' ? !isNaN(n) && isFinite(n) :
-    t == 'string' ? !n.length ? false :
-      n.length == 1 ? /\d/.test(n) :
-        /^\s*[+-]?\s*(?:(?:\d+(?:\.\d+)?(?:e[+-]?\d+)?)|(?:0x[a-f\d]+))\s*$/i.test(n) :
-      t == 'object' ? !!n && typeof n.valueOf() == 'number' && !(n instanceof Date) : false;
+  if (type === 'number') {
+    return !isNaN(value) && isFinite(value);
+
+  } else if (type === 'string') {
+    if (value.length === 0) {
+      return false;
+
+    } else if (value.length === 1) {
+      return /\d/.test(value);
+    }
+
+    const delimiter = Array.from(new Set(['.', ...additionalDelimiters]))
+      .map(d => `\\${d}`)
+      .join('|');
+
+    return new RegExp(`^[+-]?\\s*(((${delimiter})?\\d+((${delimiter})\\d+)?(e[+-]?\\d+)?)|(0x[a-f\\d]+))$`, 'i')
+      .test(value.trim());
+
+  } else if (type === 'object') {
+    return !!value && typeof value.valueOf() === 'number' && !(value instanceof Date);
+  }
+
+  return false;
+}
+/* eslint-enable jsdoc/require-description-complete-sentence */
+
+/**
+ * Checks if the passed value is numeric-like value (allows comma as delimiter).
+ *
+ * @param {*} value The value to check.
+ * @returns {boolean}
+ */
+export function isNumericLike(value) {
+  return isNumeric(value, [',']);
 }
 
 /**
